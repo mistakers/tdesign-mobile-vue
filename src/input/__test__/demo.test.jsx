687b88d450263bbf6d663ed0c19736139f3f09b9
ccccpj <ccccpj@outlook.com>
2023-03-27 13:02:16 +0800

    fix: update demo test

--- src/input/__test__/demo.test.jsx
+++ src/input/__test__/demo.test.jsx
@@ -3,19 +3,13 @@
  */
 
 import { mount } from '@vue/test-utils';
-import alignVue from '@/input/demos/align.vue';
-import limitVue from '@/input/demos/limit.vue';
 import mobileVue from '@/input/demos/mobile.vue';
-import sizeVue from '@/input/demos/size.vue';
 import specialVue from '@/input/demos/special.vue';
 import statusVue from '@/input/demos/status.vue';
 import typeVue from '@/input/demos/type.vue';
 
 const mapper = {
-  alignVue,
-  limitVue,
   mobileVue,
-  sizeVue,
   specialVue,
   statusVue,
   typeVue,
